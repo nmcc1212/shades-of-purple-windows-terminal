f9498e83e5ebd94af9b249f81f0082d77291b58a
Nial McCallister <48334675+nmcc1212@users.noreply.github.com>
2022-11-23 21:32:35 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-wget https://raw.githubusercontent.com/nmcc1212/shades-of-purple-windows-terminal/main/shades-of-purple.zsh-theme -P ~/.oh-my-zsh/themes/
-THEME="shades-of-purple"; sed -i s/^ZSH_THEME=".\+"$/ZSH_THEME=\"$THEME\"/g ~/.zshrc
-source ~/.zshrc
-echo "Edited line in ~/zshrc :" && cat ~/.zshrc | grep -m 1 ZSH_THEME
